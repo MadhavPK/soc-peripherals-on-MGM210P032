dcde4c8f55ee35d6a6719f809135907fb4e969a8
MadhavPK <madhavpk7@gmail.com>
2021-05-27 15:17:22 +0530

    Compiler generated files

--- GNU ARM v7.2.1 - Default/objects.mk
+++ GNU ARM v7.2.1 - Default/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/binapploader.o C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/platform/radio/rail_lib/autogen/librail_release/librail_config_mgm210p032jia_gcc.a C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/libmbedtls.a C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg21_gcc_release.a C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a C:/Users/madha/SimplicityStudio/v4_workspace/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/libbluetooth.a
+USER_OBJS := D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/binapploader.o D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/platform/radio/rail_lib/autogen/librail_release/librail_config_mgm210p032jia_gcc.a D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/libmbedtls.a D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/platform/radio/rail_lib/autogen/librail_release/librail_module_efr32xg21_gcc_release.a D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a D:/Study\ Material/Proj-on-GitHub/soc-peripherals-on-MGM210P032/protocol/bluetooth/lib/EFR32MG21/GCC/libbluetooth.a
 
 LIBS := -lm
 
